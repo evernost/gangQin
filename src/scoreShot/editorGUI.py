7731e71f10de3f9569fd2e4f0a4856146c0cfd36
Quentin Biache <nitrogenium@outlook.fr>
2024-10-08 00:03:23 +0200

    Exporting the GUI code to a dedicated file (2)

--- src/scoreShot/editorGUI.py
+++ src/scoreShot/editorGUI.py
@@ -136,27 +136,22 @@ class EditorGUI :
 
 
     # Keyboard bindings
-    self.captureWin.bind("<Up>", on_moveWindow)
-    self.captureWin.bind("<Down>", on_moveWindow)
-    self.captureWin.bind("<Left>", on_moveWindow)
-    self.captureWin.bind("<Right>", on_moveWindow)
-    self.captureWin.bind("<MouseWheel>", on_mouseWheel)
-    self.captureWin.bind("<Configure>", on_resize)
-    self.captureWin.bind('<KeyPress>', on_keyPress)
-    self.captureWin.bind('<KeyRelease>', on_keyRelease)
-    self.captureWin.bind("<s>", on_screenshot)
-    self.captureWin.bind("<q>", on_quit)
+    self.captureWin.bind("<Up>", self.CLBK_onMoveWindow)
+    self.captureWin.bind("<Down>", self.CLBK_onMoveWindow)
+    self.captureWin.bind("<Left>", self.CLBK_onMoveWindow)
+    self.captureWin.bind("<Right>", self.CLBK_onMoveWindow)
+    self.captureWin.bind("<MouseWheel>", self.CLBK_onMouseWheel)
+    self.captureWin.bind("<Configure>", self.CLBK_onResize)
+    self.captureWin.bind('<KeyPress>', self.CLBK_onKeyPress)
+    self.captureWin.bind('<KeyRelease>', self.CLBK_onKeyRelease)
+    self.captureWin.bind("<s>", self.CLBK_onScreenshot)
+    self.captureWin.bind("<q>", self.CLBK_onQuit)
 
-    captureListBox.bind("<<ListboxSelect>>", on_snapshotSel)
+    self.captureListBox.bind("<<ListboxSelect>>", self.CLBK_onSnapshotSel)
 
 
 
     self.ruler = ruler.Ruler(self.canvasArray)
-
-
-
-
-
 
 
 
@@ -242,7 +237,32 @@ class EditorGUI :
 
 
 
+  def CLBK_onSnapshotSel(self, event) :
+    index = self.captureListBox.curselection()
+    if index :
+      imgName = self.captureListBox.get(index)
 
+      x = ImageTk.PhotoImage(Image.open(f"{SCORE_DB_DIR}/{imgName}"))
+
+      # TODO: resize so that the picture occupies the same real estate no
+      # matter what (even if there was some screen scaling)
+      # ...
+
+
+      self.imgbox.config(image = x)
+      self.imgbox.image = x
+
+
+  def CLBK_onKeyPress(self, event) :
+    if (event.char == 'r') :
+      self.recallImg.lift()
+
+  def CLBK_onKeyRelease(self, event) :
+    if (event.char == "r") :
+      self.recallImg.lower()
+
+  def CLBK_onResize(self, event) :
+    self.rulerObj.update()
 
 
 
@@ -255,173 +275,6 @@ class EditorGUI :
   # ---------------------------------------------------------------------------
 
 
-
-
-
-      
-
-  def on_snapshotSel(event) :
-    index = captureListBox.curselection()
-    if index :
-      imgName = captureListBox.get(index)
-
-      x = ImageTk.PhotoImage(Image.open(f"{SCORE_DB_DIR}/{imgName}"))
-
-      # TODO: resize so that the picture occupies the same real estate no
-      # matter what (even if there was some screen scaling)
-      # ...
-
-
-      imgbox.config(image = x)
-      imgbox.image = x
-      
-
-  def on_keyPress(event) :
-    if (event.char == 'r') :
-      recallImg.lift()
-
-  def on_keyRelease(event) :
-    if (event.char == "r") :
-      recallImg.lower()
-
-  def on_resize(event) :
-    rulerObj.update()
-
-
-
-
-
-# -----------------------------------------------------------------------------
-# Capture window definition
-# -----------------------------------------------------------------------------
-# captureWin = tk.Toplevel(root)
-# captureWin.geometry("1250x440")
-# captureWin.title("scoreShot - Capture tool v0.1 [ALPHA] (October 2024)")
-
-# Capture window is always on top
-# captureWin.attributes("-topmost", True)
-
-# Capture window layout
-# BORDER_SIZE = 100
-# captureWin.grid_columnconfigure(0, minsize = BORDER_SIZE)
-# captureWin.grid_columnconfigure(1, weight = 1)
-# captureWin.grid_columnconfigure(2, minsize = BORDER_SIZE)
-
-# captureWin.grid_rowconfigure(0, minsize = BORDER_SIZE)
-# captureWin.grid_rowconfigure(1, weight = 1)
-# captureWin.grid_rowconfigure(2, minsize = BORDER_SIZE)
-
-
-
-
-# # Define a set of distinct colors for the 9 frames
-# colors = ["lightsteelblue", "lightblue", "lightsteelblue", "lightblue", 
-#           "red", "lightblue", "lightsteelblue", "lightblue", "lightsteelblue"]
-
-# canvasArray = []
-# for row in range(3):
-#   for col in range(3):
-#     color_index = row * 3 + col
-    
-#     # The "border" canvases have a fixed size
-#     if ((row == 0) or (row == 2) or (col == 0) or (col == 2)) :
-#       c = tk.Canvas(captureWin, bg = colors[color_index], highlightthickness = 0, width = BORDER_SIZE, height = BORDER_SIZE)
-    
-#     # The middle canvas (capture aperture) has a variable size
-#     elif ((row == 1) and (col == 1)) :
-#       c = tk.Canvas(captureWin, bg = colors[color_index], highlightthickness = 2, highlightbackground = "blue")
-#     else :
-#       c = tk.Canvas(captureWin, bg = colors[color_index], highlightthickness = 0)
-#     c.grid(row = row, column = col, sticky = "nsew")
-#     canvasArray.append(c)
-
-# # Bind the red color (unused in the palette) with the transparency property.
-# captureWin.attributes("-transparentcolor", "red")
-
-# rulerObj = ruler.Ruler(canvasArray)
-
-
-
-x = ImageTk.PhotoImage(Image.open(f"{SCORE_DB_DIR}/screenshot_0.png"))
-recallImg = tk.Label(captureWin, image = x)
-recallImg.grid(row = 1, column = 1, sticky = "nsew")
-recallImg.lower()
-
-
-# Keyboard bindings
-captureWin.bind("<Up>", on_moveWindow)
-captureWin.bind("<Down>", on_moveWindow)
-captureWin.bind("<Left>", on_moveWindow)
-captureWin.bind("<Right>", on_moveWindow)
-captureWin.bind("<MouseWheel>", on_mouseWheel)
-captureWin.bind("<Configure>", on_resize)
-captureWin.bind('<KeyPress>', on_keyPress)
-captureWin.bind('<KeyRelease>', on_keyRelease)
-captureWin.bind("<s>", on_screenshot)
-captureWin.bind("<q>", on_quit)
-
-captureListBox.bind("<<ListboxSelect>>", on_snapshotSel)
-
-captureCount = 0
-
-root.protocol("WM_DELETE_WINDOW", on_quit)
-
-
-
-
-# import tkinter as tk
-
-# class MyApp:
-#     def __init__(self, root):
-#         # Create and configure window
-#         self.root = root
-#         self.root.title("My Tkinter App")
-        
-#         # Variables for GUI state
-#         self.counter = 0
-#         self.label_text = tk.StringVar(value="Counter: 0")
-        
-#         # Create widgets
-#         self.label = tk.Label(root, textvariable=self.label_text)
-#         self.label.pack(pady=10)
-        
-#         self.increment_button = tk.Button(root, text="Increment", command=self.increment_counter)
-#         self.increment_button.pack(pady=5)
-        
-#         self.decrement_button = tk.Button(root, text="Decrement", command=self.decrement_counter)
-#         self.decrement_button.pack(pady=5)
-
-#         self.quit_button = tk.Button(root, text="Quit", command=root.quit)
-#         self.quit_button.pack(pady=20)
-        
-#         # Bind events (e.g., keyboard and mouse)
-#         root.bind("<Up>", self.on_key_press_up)
-#         root.bind("<Down>", self.on_key_press_down)
-    
-#     def increment_counter(self):
-#         self.counter += 1
-#         self.update_label()
-    
-#     def decrement_counter(self):
-#         self.counter -= 1
-#         self.update_label()
-    
-#     def update_label(self):
-#         self.label_text.set(f"Counter: {self.counter}")
-    
-#     def on_key_press_up(self, event):
-#         """Callback for Up arrow key press."""
-#         self.increment_counter()
-    
-#     def on_key_press_down(self, event):
-#         """Callback for Down arrow key press."""
-#         self.decrement_counter()
-
-# # Main application loop
-# if __name__ == "__main__":
-#     root = tk.Tk()
-#     app = MyApp(root)
-#     root.mainloop()
 
 
 
